ffecb1a534e64a583369e4ff7e69cd00e6a2145d
Mbishy <mbishy@yandex.ru>
2024-05-21 16:15:37 +0500

    add txt files and method

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,9 +1,43 @@
 package org.example;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+import java.io.File;
+import java.util.ArrayList;
+import java.io.FileNotFoundException;
+
+
+
 public class Main {
     public static void main(String[] args) {
-
-        System.out.println("help me");
-        System.out.println("new version ");
+        selectionWord();
     }
+
+    public static void beginGame(){
+
+    }
+
+    public static void drawGallows(){
+
+    }
+
+    public static void turnUser(){
+
+    }
+
+    public static void selectionWord(){
+        List<String> lines = new ArrayList<>();
+        try (Scanner scanner = new Scanner(new File("list1.txt"))){
+            while (scanner.hasNextLine()) {
+                String line = scanner.nextLine();
+                lines.add(line);
+            }
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+        System.out.println(lines);
+        System.out.println(lines.get(3));
+    }
+
 }
