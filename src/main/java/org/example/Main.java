cc93bde5202b68e1f1e173517272d5134bb6675f
Mbishy <mbishy@yandex.ru>
2024-06-18 16:59:49 +0500

    111

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -15,87 +15,111 @@ public class Main {
 
     public static String randomWorld;
     int errorPionts;
+
     public static void main(String[] args) {
+        beginGame();
         drawGallows(0);
         selectionWord();
         hidingWorld();
     }
 
-    public static void beginGame(){
+    public static void beginGame() {
+        System.out.println("Добро пожаловать!");
+        System.out.println("Для начала игры нажмите(Н), для выхода из игры(В)");
+        Scanner scanner = new Scanner(System.in);
+        String button = scanner.nextLine();
+        switch (button) {
+            case "Н":
+                System.out.println("Начинаем новую игру");
+                break;
+
+            case "В":
+                System.out.println("Выходим из игры");
+                break;
+
+            default:
+                System.out.println("Вы ввели не тот символ");
+        }
 
     }
 
-    public static void drawGallows(int errorPoints){
+    public static void drawGallows(int errorPoints) {
         switch (errorPoints) {
-            case 0: System.out.println("\n------" +
-                    "\n| " +
-                    "\n|" +
-                    "\n|" +
-                    "\n|" +
-                    "\n|" +
-                    "\n|" +
-                    "\n~~~~~~~~\n");
+            case 0:
+                System.out.println("\n------" +
+                        "\n| " +
+                        "\n|" +
+                        "\n|" +
+                        "\n|" +
+                        "\n|" +
+                        "\n|" +
+                        "\n~~~~~~~~\n");
                 break;
 
-            case 1: System.out.println("\n------" +
-                    "\n|   | " +
-                    "\n|" +
-                    "\n|" +
-                    "\n|" +
-                    "\n|" +
-                    "\n|" +
-                    "\n~~~~~~~~\n");
-            break;
-
-            case 2: System.out.println("\n------" +
-                    "\n|   | " +
-                    "\n|   0" +
-                    "\n|" +
-                    "\n|" +
-                    "\n|" +
-                    "\n|" +
-                    "\n~~~~~~~~\n");
+            case 1:
+                System.out.println("\n------" +
+                        "\n|   | " +
+                        "\n|" +
+                        "\n|" +
+                        "\n|" +
+                        "\n|" +
+                        "\n|" +
+                        "\n~~~~~~~~\n");
                 break;
 
-            case 3: System.out.println("\n------" +
-                    "\n|   | " +
-                    "\n|   0" +
-                    "\n|  /|\\ " +
-                    "\n|" +
-                    "\n|" +
-                    "\n|" +
-                    "\n~~~~~~~~\n");
+            case 2:
+                System.out.println("\n------" +
+                        "\n|   | " +
+                        "\n|   0" +
+                        "\n|" +
+                        "\n|" +
+                        "\n|" +
+                        "\n|" +
+                        "\n~~~~~~~~\n");
                 break;
 
-            case 4: System.out.println("\n------" +
-                    "\n|   | " +
-                    "\n|   0" +
-                    "\n|  /|\\ " +
-                    "\n|   |" +
-                    "\n|" +
-                    "\n|" +
-                    "\n~~~~~~~~\n");
+            case 3:
+                System.out.println("\n------" +
+                        "\n|   | " +
+                        "\n|   0" +
+                        "\n|  /|\\ " +
+                        "\n|" +
+                        "\n|" +
+                        "\n|" +
+                        "\n~~~~~~~~\n");
                 break;
 
-            case 5: System.out.println("\n------" +
-                    "\n|   | " +
-                    "\n|   0" +
-                    "\n|  /|\\ " +
-                    "\n|   |" +
-                    "\n|  / \\ " +
-                    "\n|" +
-                    "\n~~~~~~~~\n");
+            case 4:
+                System.out.println("\n------" +
+                        "\n|   | " +
+                        "\n|   0" +
+                        "\n|  /|\\ " +
+                        "\n|   |" +
+                        "\n|" +
+                        "\n|" +
+                        "\n~~~~~~~~\n");
+                break;
+
+            case 5:
+                System.out.println("\n------" +
+                        "\n|   | " +
+                        "\n|   0" +
+                        "\n|  /|\\ " +
+                        "\n|   |" +
+                        "\n|  / \\ " +
+                        "\n|" +
+                        "\n~~~~~~~~\n");
                 break;
         }
     }
 
-    public static void turnUser(){
+    public static void turnUser() {
 
     }
 
-    public static void selectionWord(){
+    public static void selectionWord() {
         List<String> lines = new ArrayList<>();
-        try (Scanner scanner = new Scanner(new File("list1.txt"))){
+        try (Scanner scanner = new Scanner(new File("list1.txt"))) {
             while (scanner.hasNextLine()) {
                 String line = scanner.nextLine();
                 lines.add(line);
@@ -106,7 +130,7 @@ public class Main {
         randomWorld = (lines.get((int) (Math.random() * lines.size())));
     }
 
-    public static void hidingWorld(){
+    public static void hidingWorld() {
         System.out.println(randomWorld);
     }
 
